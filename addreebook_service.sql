fd61580d7a542b65b824aff9a6af3afa9618120d
mehulbhange <bhangemehul@gmail.com>
2022-09-14 20:13:28 +0530

    ADDED:[Mehul] added query to delete record

--- addreebook_service.sql
+++ addreebook_service.sql
@@ -30,3 +30,22 @@ Rows matched: 1  Changed: 1  Warnings: 0
 | Mehul      | Bhange    | mumbai  | mumbai | maharashtra | 123456 | 9090909090   | mehul@gmail.com |
 +------------+-----------+---------+--------+-------------+--------+--------------+-----------------+
 
+/* UC5 */
+/* Table before deleting data */
++------------+-----------+---------+--------+-------------+--------+--------------+------------------+
+| first_name | last_name | address | city   | state       | zip    | phone_number | email            |
++------------+-----------+---------+--------+-------------+--------+--------------+------------------+
+| Mehul      | Bhange    | mumbai  | mumbai | maharashtra | 123456 | 9090909090   | mehul@gmail.com  |
+| Hitesh     | Meher     | pune    | pune   | maharashtra | 123456 | 9090909090   | hitesh@gmail.com |
++------------+-----------+---------+--------+-------------+--------+--------------+------------------+
+
+mysql> DELETE FROM address_book WHERE first_name = 'Hitesh';
+Query OK, 1 row affected (0.01 sec)
+
+/* Table after deleting data */
++------------+-----------+---------+--------+-------------+--------+--------------+-----------------+
+| first_name | last_name | address | city   | state       | zip    | phone_number | email           |
++------------+-----------+---------+--------+-------------+--------+--------------+-----------------+
+| Mehul      | Bhange    | mumbai  | mumbai | maharashtra | 123456 | 9090909090   | mehul@gmail.com |
++------------+-----------+---------+--------+-------------+--------+--------------+-----------------+
+
